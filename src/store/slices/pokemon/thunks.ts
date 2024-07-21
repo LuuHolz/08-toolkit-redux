c56f3572be337f407cce475be9817885e7bdb526
Luciana <luciana.holzmann@gmail.com>
2024-07-22 01:42:09 +0200

    [FEAT] Axios

--- src/store/slices/pokemon/thunks.ts
+++ src/store/slices/pokemon/thunks.ts
@@ -1,4 +1,5 @@
 import { startLoadingPokemons, setPokemons } from './pokemonSlice';
+import { pokemonApi } from '../../../api/pokemonApi';
 
 
 export const getPokemons = (  page = 0 ) => {
@@ -6,10 +7,16 @@ export const getPokemons = (  page = 0 ) => {
         dispatch( startLoadingPokemons() )
 
         //realizar peticion http con AXIOS
+
+        //con FECH
+        // const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
+        // const data = await respuesta.json();
+        // console.log(data)
         
+        //con AXIOS (pokemonApi.ts)
+        const { data }  = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`)
 
 
-
-        // dispatch( setPokemons() )
+        dispatch( setPokemons({ pokemons: data.results, data: page + 1 }) )
     }
 }
